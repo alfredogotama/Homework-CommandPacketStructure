c058b2280850977ead97b5edb3f72e3f53f438c7
alfredogotama <aany@gmu.edu>
2015-04-30 10:26:20 -0400

    Create UserStruct.py

---
+++ UserStruct.py
@@ -0,0 +1,19 @@
+import commandstruct as cst
+import time
+import socket
+import pickle
+
+
+IP = "127.0.0.1"
+PORT = 5005
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #TCP SEND
+sock.connect((IP, PORT))
+
+ref = cst.ROBOT_JOINT_REF()
+
+ref.position = 1
+ref.command = 1
+#ref.pack()
+
+sock.sendall(ref)
